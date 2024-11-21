38f2dc8d7b974195a583437985810a2b7faff5e8
Cornelius Tiefenmoser <c.tiefenmoser@gmail.com>
2024-11-21 13:01:24 +0100

    Wurde gerefactored um klassen Zu nutzen. Macht hier den code später glaub schöner. TCP Header structure fertig muss bloß die tatsächlicen werte noch überprüfen.

--- src/main/java/de/uulm/in/vs/grn/p3a/Sockagram.java
+++ src/main/java/de/uulm/in/vs/grn/p3a/Sockagram.java
@@ -9,34 +9,41 @@ import java.net.URL;
 import java.nio.ByteBuffer;
 
 public class Sockagram {
+    private int seqNum;
+    private int ackNum;
+    private final Socket socket;
+    private final short srcPort;
+    private final short destPort;
 
-    public static void main(String[] args) throws Exception {
-        byte[] bytes = ByteBuffer.allocate(2).putShort((short) 10).array();
-        for (byte b : bytes) {
-            System.out.format("0x%x ", b);
-        }
-
-
-
-        /*
-        //error handling ist für anfänger
-        Socket socket = new Socket("vns.lxd-vs.uni-ulm.de",7777); // TODO: Muss inputs als adressen und port nutzen
-        OutputStream out = socket.getOutputStream();// MUSS MANUEL GEFLUSHED WERDEN!!
-        InputStream in = socket.getInputStream();
-
-        //Seperate requestHeader and data is easier to work with
-        byte[] requestHeader =constructTCPSegmentHeader(socket.getLocalPort(),socket.getPort());
-        byte[] data = null;
-
-        out.write(requestHeader);
-        out.write(data);
-        out.flush();
-        */
+    public Sockagram(String host, int port) throws Exception{
+        //TODO gib den sinnvolle werte
+        this.seqNum = 0;
+        this.ackNum = 0;
+        this.socket = new Socket(host,port); // definier die OutputStreams lokal da sonst der Inhalt Unpredictable wird(nvm vllt doch anders idk)
+        this.srcPort = (short) this.socket.getLocalPort();
+        this.destPort = (short) this.socket.getPort();
     }
 
-    private static byte[] constructTCPSegmentHeader(int srcPort, int destPort) throws IOException {
-        //Construct the segment as a Bytebuffer
-        ByteBuffer segement = ByteBuffer.allocate(20);
+    public void main(String[] args) throws Exception {
+        //error handling ist für anfänger
+        Sockagram sock = new Sockagram("vns.lxd-vs.uni-ulm.de",7777); // TODO: Muss inputs als adressen und port nutzen
+        sock.sendMessage(constructTCPSegmentHeader(false,false,false),null);
+
+
+    }
+    //muss noch schauen ob ich das nur für den Handshake nutze oder auch für mehr
+    public void sendMessage(byte[] requestHeader,byte[] data){
+        try (OutputStream out = this.socket.getOutputStream()) {
+            out.write(requestHeader);
+            out.write(data);
+            out.flush();
+        }catch (Exception e){
+
+        }
+    }
+
+    //Das ist eigentlich echt dumm wie ich das mache aber ich hab kein bock mehr das umzuschreiben
+    private byte[] constructTCPSegmentHeader(Boolean ackBit, Boolean synBit, Boolean finBit) throws IOException {
 
         /* TCP Segment Header Structure:
         *
@@ -53,34 +60,27 @@ public class Sockagram {
         * The Tcp standard features a bit more but we dont use options at all and its allowed to be len 0
         * The Only thing missing which we use is the Data but we create that in a different Function.
         */
-        int seqNum = 0;//sollte global sein
-        segement.putShort((short) srcPort);
-        segement.putShort((short) destPort);
-        segement.putInt(seqNum);
+        //Construct the segment as a Bytebuffer with the size we defined above
+        ByteBuffer segment = ByteBuffer.allocate(20);
 
-        return null;
+        //---First Line
+        segment.putShort((short) srcPort);
+        segment.putShort((short) destPort);
+        //---Second Line
+        segment.putInt(this.seqNum);
+        segment.putInt(this.ackNum);
+        //--- Third Line
+        segment.put((byte) 0); //set Dataoffset and reserved to zero
+        //set the flags
+        int flags = 0; //int macht mehr sinn da sonst bei jeder operation zu byte ge typcasted werden muss
+        flags = ackBit ? flags | (1 << 4) : flags;//TODO : schauen ob die shifts überhaupt passen
+        flags = synBit ? flags | (1 << 1) : flags;
+        flags = finBit ? flags | (1 << 0) : flags;
+        segment.put((byte) flags);
+        segment.putShort((short) 0); //TODO: sets the window Size
+        //Fourth Line
+        segment.putShort((short) 0); //TODO: checksum
+        segment.putShort((short) 0); // Urgend Data bit (not used by us)
+        return segment.array();
     }
-
-    private static byte[] intToByteArray(int in,int len){
-        //TODO: Das ist sogar zu Cursed für mich
-        // Das ist eine sünde gegen gott aber BITE ME God Is dead and we have killed him
-        ByteArrayOutputStream arr = new ByteArrayOutputStream();
-        //TODO ensure bite length by prepending 0
-        String str = Integer.toBinaryString(in);
-        for(int i = 0; i< len ;i++){
-            arr.write(Byte.parseByte(str.substring(i*8,i*8+8)));//das sollte passen aber könnte auch violently explodieren
-        }
-        return arr.toByteArray();
-    }
-    private static byte[] stringToByteArray(String strIn,int len){
-        ByteBuffer byteBuffer = ByteBuffer.allocate(len);
-        StringBuilder str = new StringBuilder(strIn);
-        //Prepends Zeros so its always multiples of 8 long
-        str.insert(0,str.repeat("0",strIn.length()%8));
-        for(int i = 0; i < str.length()/8;i++){
-            byteBuffer.put(Byte.parseByte(str.substring(i*8,(i*8+8)),2));//why the fuck are 8 bits too much for a byte???
-        }
-        return byteBuffer.array();
-    }
-
 }
